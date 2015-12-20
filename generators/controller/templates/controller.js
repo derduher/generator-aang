3f3d01933174fc813bc4870eaa83795d7393801c
Patrick Weygand <patrick.weygand@gmail.com>
2015-12-20 14:54:30 -0800

    controller sans generator for test

---
+++ generators/controller/templates/controller.js
@@ -0,0 +1,7 @@
+angular.module('<%= moduleName %>').controller('<%= controllerName %>', [
+  class <%= controllerName %> {
+    constructor (
+    ) {
+    }
+  }
+])
--- test/controller.js
+++ test/controller.js
@@ -1,20 +1,55 @@
-'use strict';
+'use strict'
 
-var path = require('path');
-var assert = require('yeoman-generator').assert;
-var helpers = require('yeoman-generator').test;
+var path = require('path')
+var fs = require('fs-extra')
+var assert = require('yeoman-generator').assert
+var helpers = require('yeoman-generator').test
 
 describe('Aang:generators/controller', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/controller'))
+  describe('things', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/controller'))
+      .inTmpDir(function (dir) {
+        fs.copySync(path.join(__dirname, '../templates/common'), dir)
+      })
       .withArguments('name')
+      .withPrompts({module: 'com.project'})
       .withOptions({ skipInstall: true, force: true })
-      .on('end', done);
-  });
+      .on('end', done)
+    })
 
-  it('creates files', function () {
-    assert.file([
-      'somefile.js'
-    ]);
-  });
-});
+    it('creates the controller in the base module location', function () {
+      assert.file([
+        'app/assets/javascripts/controllers/NameController.es6'
+      ])
+    })
+
+    it('has file name, module filled out', function () {
+      assert.fileContent('app/assets/javascripts/controllers/NameController.es6', /angular\.module\('com\.project/)
+      assert.fileContent('app/assets/javascripts/controllers/NameController.es6', /controller\('NameController/)
+    })
+  })
+  describe('yo fs \'name controller\' --module com.project.module', function () {
+    before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/controller'))
+      .inTmpDir(function (dir) {
+        fs.copySync(path.join(__dirname, '../templates/common'), dir)
+        console.log(dir)
+      })
+      .withArguments('NameController')
+      .withOptions({ module: 'com.project.module' })
+      .on('end', done)
+    })
+
+    it('creates the file in a subfolder of the root', function () {
+      assert.file([
+        'app/assets/javascripts/module/controllers/NameController.es6'
+      ])
+    })
+
+    it('has file name, module filled out', function () {
+      assert.fileContent('app/assets/javascripts/module/controllers/NameController.es6', /angular\.module\('com\.project.module/)
+      assert.fileContent('app/assets/javascripts/module/controllers/NameController.es6', /controller\('NameController/)
+    })
+  })
+})
