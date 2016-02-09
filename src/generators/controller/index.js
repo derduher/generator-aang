c72c8ca2c5e2d035701eb3ecaf7d9746a1d2917f
Patrick Weygand <patrick.weygand@gmail.com>
2016-02-09 13:37:32 -0800

    almost done getting undefined config values to trigger a prompt

--- src/generators/controller/index.js
+++ src/generators/controller/index.js
@@ -4,11 +4,13 @@ import Aang from '../../lib/aang/index'
 export default class Controller extends Aang {
   constructor (args, options) {
     super(args, options)
+    console.log('constr')
 
     this._normalizeName('Controller')
   }
 
   prompting (...args) {
+    console.log('prompting')
     super.prompting(...args)
   }
 
--- src/lib/aang/index.js
+++ src/lib/aang/index.js
@@ -3,15 +3,6 @@ const changeCase = require('change-case')
 import { NamedBase } from 'yeoman-generator'
 
 export default class Aang extends NamedBase {
-  _initConfig () {
-    // pass defaults
-    // TODO: what if not defined
-    var pkgjson = this.fs.readJSON(this.destinationPath('package.json'))
-    // TODO: what if not defined
-    var defaults = pkgjson.config['generator-aang']
-    this.config.defaults(defaults)
-    this.config.save()
-  }
   _normalizeName (suffix, instance) {
     var origArg = this.name
 
@@ -87,9 +78,6 @@ export default class Aang extends NamedBase {
 
   constructor (args, options) {
     super(args, options)
-    // Configure Lodash templating so it ignores interpolation markers in
-    // ES6 template strings.
-    this._initConfig()
 
     this.option('module', {
       desc: 'angular module name. Will be used for folder too.',
@@ -99,20 +87,17 @@ export default class Aang extends NamedBase {
 
     this.option('rootModule', {
       desc: 'default module that other modules namespace off of',
-      type: String,
-      defaults: this.config.get('rootModule')
+      type: String
     })
 
     this.option('rootModulePath', {
       desc: 'path to scripts',
-      type: String,
-      defaults: this.config.get('paths').scripts
+      type: String
     })
 
     this.option('rootTestPath', {
       desc: 'path to tests',
-      type: String,
-      defaults: this.config.get('paths').specs
+      type: String
     })
 
     this.option('modulePath', {
@@ -127,20 +112,17 @@ export default class Aang extends NamedBase {
 
     this.option('sourceExtension', {
       desc: 'override to package default extension for source',
-      type: String,
-      defaults: this.config.get('extensions').source
+      type: String
     })
 
     this.option('unitExtension', {
       desc: 'override to package default extension for unit tests',
-      type: String,
-      defaults: this.config.get('extensions').unit
+      type: String
     })
 
     this.option('e2eExtension', {
       desc: 'override to package default extension for e2e tests',
-      type: String,
-      defaults: this.config.get('extensions').e2e
+      type: String
     })
 
     this.option('fileCase', {
@@ -151,21 +133,78 @@ export default class Aang extends NamedBase {
   }
 
   prompting () {
-    var done = this.async()
-    if (!this.options.module) {
-      this.prompt({
+    console.log('promptinglib')
+    const done = this.async()
+    let prompts = [
+      {
         type: 'input',
-        name: 'module',
-        message: 'module you would like to install this under',
+        name: 'rootModule',
+        message: 'what is your application\'s main module',
         store: true
-      }, function (answers) {
-        this.options.module = answers.module
-        this._setModulePath()
-        done()
-      }.bind(this))
-    } else {
-      this._setModulePath()
-      done()
+      },
+      {
+        type: 'input',
+        name: 'rootModulePath',
+        message: 'what is the path to the root of your angular src',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'rootTestPath',
+        message: 'what is the path to the root of your unit tests',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'sourceExtension',
+        message: 'what extension would you like to use for your javascript (eg. js)',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'unitExtension',
+        message: 'what extension would you like to use for your unit tests (eg. js)',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'e2eExtension',
+        message: 'what extension would you like to use for your e2e tests (eg. js)',
+        store: true
+      }
+    ]
+
+    prompts = prompts.filter(opt => {
+      if (this.config.get(opt.name) && !this.options[opt.name]) {
+        this.options[opt.name] = this.config.get(opt.name)
+        return false
+      }
+      return true
+    })
+
+    if (prompts.length) {
+      this.prompt(prompts, answers => {
+        for (let opt in answers) {
+          this.options[opt] = answers[opt]
+        }
+        this.config.set(answers)
+
+        if (!this.options.module) {
+          this.prompt({
+            type: 'input',
+            name: 'module',
+            message: 'module you would like to install this under',
+            store: true
+          }, answers => {
+            this.options.module = answers.module
+            this._setModulePath()
+            done()
+          })
+        } else {
+          this._setModulePath()
+          done()
+        }
+      })
     }
   }
 }
